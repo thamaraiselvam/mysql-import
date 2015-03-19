1accc66d418eccaddc72661a439185f97f01cd55
Thamaraiselvam <thamaraiselvam@live.com>
2015-03-19 12:32:58 +0530

    Create import.php

---
+++ import.php
@@ -0,0 +1,41 @@
+<?php
+// Name of the file
+$filename = 'db-backup-1426746323-fa5bfef761fada0cc5531454fec94cad.sql';
+// MySQL host
+$mysql_host = 'localhost';
+// MySQL username
+$mysql_username = 'root';
+// MySQL password
+$mysql_password = '';
+// Database name
+$mysql_database = 'test';
+
+// Connect to MySQL server
+mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+// Select database
+mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
+
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+if (substr($line, 0, 2) == '--' || $line == '')
+    continue;
+
+// Add this line to the current segment
+$templine .= $line;
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // Perform the query
+    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
+ echo "Tables imported successfully";
+?>
